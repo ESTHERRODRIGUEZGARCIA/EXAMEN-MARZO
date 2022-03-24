7e2f69e6e3b15709baaad95a87a0ec7976f8c320
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-03-24 09:03:09 +0100

    now hecho

---
+++ ejerbanca.py
@@ -0,0 +1,28 @@
+
+import random
+from datetime import datetime
+
+
+class cuentabancaria():
+
+
+    def __init__(self, id, fecha, nombre, modo, saldo):
+        self.id = id
+        self.fecha = fecha
+        self.nombre = nombre
+        self.modo = modo
+        self.saldo = saldo
+
+    def crearcuenta():
+        print("Bienvenido. Usted va a crear una cuenta bancaria.")
+        nombre = str(input("Introduzca su nombre por favor: "))
+        global A
+        global E
+        modo = int(input("1. Cuenta a plazo fijo/n2 : Cuenta VIP "))
+        numero = str(random.randint(100000000000, 999999999999))
+        now = datetime.now()
+        A = cuentabancaria(str(random.randint(100000, 999999)), nombre, datetime.now(),numero, 10000, modo)
+        print("Hola " + nombre + ". Ha creado una cuenta el día " + str(datetime.now()) + " con el número de cuenta: " + numero)
+        E = cuentabancaria(str(random.randint(100000, 999999)), "Segunda cuenta", datetime.now(), str(random.randint(100000000000, 999999999999)), 10000, 5)
+        print("La segunda cuenta con la que va a poder operar ha sido creada por defecto a nombre de Esther, con un saldo también de 10000 euros y fue creada el 15/07/2014, es una cuenta VIP")
+
