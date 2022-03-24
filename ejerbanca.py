d82df5c67938432e4b486ba6e7c293f3172d6e91
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-03-24 09:30:21 +0100

    DEF RETIRAR terminado...

--- ejerbanca.py
+++ ejerbanca.py
@@ -13,6 +13,37 @@ class cuentabancaria():
         self.modo = modo
         self.saldo = saldo
 
+    def setid(self, id):
+        self.id = id
+
+    def getid(self):
+        return(self.id)
+
+    def setfecha(self, fecha):
+        self.fecha = fecha
+
+    def getfecha(self):
+        return(self.fecha)
+
+    def setnombre(self, nombre):
+        self.nombre = nombre
+
+    def getnombre(self):
+        return (self.nombre)
+
+    def setmodo(self, modo):
+        self.modo = modo
+
+    def getmodo(self):
+        return (self.modo)
+
+    def setsaldo(self, saldo):
+        self.saldo = saldo
+
+    def getsaldo(self):
+            return self.saldo
+
+
     def crearcuenta():
         print("Bienvenido. Usted va a crear una cuenta bancaria.")
         nombre = str(input("Introduzca su nombre por favor: "))
@@ -27,12 +58,22 @@ class cuentabancaria():
         print("La segunda cuenta ha sido asociada a una cuenta VIP. También tiene un saldo inicial de 10.000 € ")
 
     crearcuenta()
+
+
     def RETIRAR():
         dineroretirado=int(input("Usted ha elegido retirar dinero./n¿Cuánto desea retirar de su cuenta?: "))
-        
+        if dineroretirado > 0  and dineroretirado <= self.saldo:
+            print("Han sido retirados ", dineroretirado, " €.")
+            print("Ctualmente dispone de un saldo de ", self.saldo, " €.")
+            nuevaop = int(input("¿Quiere realizar más operaciones?/nSí: pulse 1 /nNo: pulse 2 "))
+            if nuevaop == 1:
+                pass #falta algo!!!
+            else:
+                exit()
 
-
-
+        else:
+            print("Lo sentimos. No puede retirar más dinero del que tiene su cuenta.Introduzca correctamente cuánto quiere retirar de su cuenta.")
+            cuentabancaria.RETIRAR(A)
 
 cuentabancaria()
 
