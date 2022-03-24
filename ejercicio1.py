96268c5cbe802bbd52e50c82faad7e2ab90af105
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-03-24 09:00:37 +0100

    commits fecha de hoy

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,10 +1,11 @@
 #ejercicio1: Escriba una clase que permita describir un libro y situar los valores asociados. Dar un ejemplo de uso en Python.
 class libro():
-    def __init__(self, autor, fecha, pags, tema):
+    def __init__(self, nombre, autor, fecha, pags, genero):
+        self.nombre= nombre
         self.autor = autor
         self.fecha = fecha
         self.pags = pags
-        self.tema = tema
+        self.genero = genero
 
     def autor():
-        print("El autor del libro es ")
+        autor = int("El autor del libro es Esther Rodríguez")
